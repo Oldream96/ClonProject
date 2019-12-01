8e87d7cd227cb2739446f190ede73557e490fb99
Diego <diegocont18@gmail.com>
2019-11-30 23:35:40 -0500

    commit1

--- app/src/main/java/pe/com/hatunsol/hatunsolmovil/api/service/ApiUtils.java
+++ app/src/main/java/pe/com/hatunsol/hatunsolmovil/api/service/ApiUtils.java
@@ -7,8 +7,8 @@ public class ApiUtils {
     public ApiUtils() {
     }
 
-    public static final String API_URL = "http://192.168.1.154:16616/";
-//public static final String API_URL = "http://www.hatunsol.com.pe/ws_hatun_Testing/";
+//    public static final String API_URL = "http://192.168.1.154:16616/";
+public static final String API_URL = "http://www.hatunsol.com.pe/ws_hatun_Testing/";
     public static Services getService(){
         return ApiRetrofit.getClient(API_URL).create(Services.class);
     }
